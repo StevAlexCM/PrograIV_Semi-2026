d01b0121c539792db007f1d41cff2cc129e1f255
StevAlexCM <campossteven593@gmail.com>
2026-02-03 14:06:39 -0600

    Tarea

--- main.js
+++ main.js
@@ -1,74 +1,106 @@
-document.addEventListener("DOMContentLoaded", () => {
-    mostrarAlumnos();
-    frmAlumnos.addEventListener("submit", (e) => {
-        e.preventDefault();
-       guardarAlumno();
-    });
-});
+const { createApp } = Vue;
 
-function mostrarAlumnos(){
-    let tblAlumnos = document.querySelector("#tblAlumnos");
-    let n = localStorage.length;
-    tblAlumnos.innerHTML = "";
-    let filas = "";
-    for(let i = 0; i < n; i++){
-        let key = localStorage.key(i);
-        if (Number(key)){
-        let data = JSON.parse(localStorage.getItem(key));
-        if (data != null){
-            filas += `
-            <tr>
-                <td>${data.codigo}</td>
-                <td>${data.nombre}</td>
-                <td>${data.direccion}</td>
-                <td>${data.email}</td>
-                <td>${data.telefono}</td>
-                <td>
-                    <button type="button" class="btn btn-warning">Editar</button>
-                    <button type="button" class="btn btn-danger">Eliminar</button>
-                </td>
-            </tr>`;
+createApp({
+    data(){
+        return{
+            alumno:{
+                codigo:"",
+                nombre:"",
+                direccion:"",
+                municipio:"",
+                departamento:"",
+                telefono:"",
+                fechaNacimiento:"",
+                sexo:""
+            },
+            accion:'nuevo',
+            id:0,
+            buscar:'',
+            alumnos:[]
         }
+    },
+    methods:{
+        obtenerAlumnos(){
+            let n = localStorage.length;
+            this.alumnos = [];
+            for(let i=0; i<n; i++){
+                let key = localStorage.key(i);
+                if( Number(key) ){
+                    let data = JSON.parse(localStorage.getItem(key));
+                    if( data.nombre.toUpperCase().includes(this.buscar.toUpperCase()) || 
+                        data.codigo.toUpperCase().includes(this.buscar.toUpperCase()) ){
+                        this.alumnos.push(data);
+                    }
+                }
+            }
+        },
+        eliminarAlumno(id, e){
+            e.stopPropagation();
+            if(confirm("¿Está seguro de eliminar el alumno?")){
+                localStorage.removeItem(id);
+                this.obtenerAlumnos();
+            }
+        },
+        modificarAlumno(alumno){
+            this.accion = 'modificar';
+            this.id = alumno.id;
+            this.alumno.codigo = alumno.codigo;
+            this.alumno.nombre = alumno.nombre;
+            this.alumno.direccion = alumno.direccion;
+            this.alumno.municipio = alumno.municipio;
+            this.alumno.departamento = alumno.departamento;
+            this.alumno.telefono = alumno.telefono;
+            this.alumno.fechaNacimiento = alumno.fechaNacimiento;
+            this.alumno.sexo = alumno.sexo;
+        },
+        guardarAlumno() {
+            let datos = {
+                id: this.accion=='modificar' ? this.id : this.getId(),
+                codigo: this.alumno.codigo,
+                nombre: this.alumno.nombre,
+                direccion: this.alumno.direccion,
+                municipio: this.alumno.municipio,
+                departamento: this.alumno.departamento,
+                telefono: this.alumno.telefono,
+                fechaNacimiento: this.alumno.fechaNacimiento,
+                sexo: this.alumno.sexo
+            }, codigoDuplicado = this.buscarAlumno(datos.codigo);
+            if(codigoDuplicado && this.accion=='nuevo'){
+                alert("El codigo del alumno ya existe, "+ codigoDuplicado.nombre);
+                return; //Termina la ejecucion de la funcion
+            }
+            localStorage.setItem( datos.id, JSON.stringify(datos));
+            this.limpiarFormulario();
+            this.obtenerAlumnos();
+        },
+        getId(){
+            return new Date().getTime();
+        },
+        limpiarFormulario(){
+            this.accion = 'nuevo';
+            this.id = 0;
+            this.alumno.codigo = '';
+            this.alumno.nombre = '';
+            this.alumno.direccion = '';
+            this.alumno.municipio = '';
+            this.alumno.departamento = '';
+            this.alumno.telefono = '';
+            this.alumno.fechaNacimiento = '';
+            this.alumno.sexo = '';
+        },
+        buscarAlumno(codigo=''){
+            let n = localStorage.length;
+            for(let i = 0; i < n; i++){
+                let key = localStorage.key(i);
+                let datos = JSON.parse(localStorage.getItem(key));
+                if(datos?.codigo && datos.codigo.trim().toUpperCase() == codigo.trim().toUpperCase()){
+                    return datos;
+                }
+            }
+            return null;
         }
+    },
+    mounted(){
+        this.obtenerAlumnos();
     }
-    tblAlumnos.innerHTML = filas;
-}
-
-function guardarAlumno() {
-    let datos = {
-        id: getId(),
-        codigo: txtCodigoAlumno.value,
-        nombre: txtnombreAlumno.value,
-        direccion: txtDireccionAlumno.value,
-        email: txtEmailAlumno.value,
-        telefono: txtTelefonoAlumno.value
-    }, codigoDuplicado = buscarAlumno(datos.codigo);
-    if(codigoDuplicado){
-        alert("El codigo del alumno ya existe, "+ codigoDuplicado.nombre);
-        return;
-    }
-    localStorage.setItem( datos.id, JSON.stringify(datos)); 
-
-    mostrarAlumnos();
-    limpiarFormulario();
-}
-
-function getId(){
-    return localStorage.length + 1;
-}
-
-function limpiarFormulario(){
-    frmAlumnos.reset();
-}
-
-function buscarAlumno(codigo=''){
-    let n = localStorage.length;
-    for(let i = 0; i < n; i++){
-        let key = localStorage.key(i);
-        let datos = JSON.parse(localStorage.getItem(key));
-        if(datos?.codigo && datos.codigo.trim().toUpperCase() == codigo.trim().toUpperCase()){
-            return datos;
-        }
-    }
-    return null;
-}
+}).mount("#app");
